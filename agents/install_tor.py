3542bacdbdfa5ce2f662a393622593a9c7f194bf
Edd <33794654+giteed@users.noreply.github.com>
2025-01-12 10:00:29 +0300

    Update install_tor.py

--- agents/install_tor.py
+++ agents/install_tor.py
@@ -8,30 +8,39 @@ import sys
 def check_root():
     return os.geteuid() == 0
 
+def run_command(command):
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка при выполнении команды {' '.join(command)}: {e}")
+        sys.exit(1)
+
 def install_tor():
     print("Начинаю установку Tor...")
     try:
-        subprocess.run(["rpm", "--import", "https://www.elrepo.org/RPM-GPG-KEY-elrepo.org"], check=True)
-        subprocess.run(["dnf", "clean", "all"], check=True)
-        subprocess.run(["dnf", "install", "-y", "epel-release"], check=True)
-        subprocess.run(["dnf", "update", "-y", "--refresh"], check=True)
-        subprocess.run(["dnf", "install", "-y", "tor"], check=True)
+        # Удаляем старый ключ и репозиторий ELRepo
+        run_command(["rm", "-f", "/etc/pki/rpm-gpg/RPM-GPG-KEY-elrepo.org"])
+        run_command(["rm", "-f", "/etc/yum.repos.d/elrepo.repo"])
+        
+        # Устанавливаем новый ключ и репозиторий ELRepo
+        run_command(["rpm", "--import", "https://www.elrepo.org/RPM-GPG-KEY-elrepo.org"])
+        run_command(["dnf", "install", "-y", "https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm"])
+        
+        run_command(["dnf", "clean", "all"])
+        run_command(["dnf", "install", "-y", "epel-release"])
+        run_command(["dnf", "update", "-y", "--refresh"])
+        run_command(["dnf", "install", "-y", "tor"])
         print("Tor успешно установлен.")
-        subprocess.run(["firewall-cmd", "--add-service=tor", "--permanent"], check=True)
-        subprocess.run(["firewall-cmd", "--reload"], check=True)
-
-    except subprocess.CalledProcessError as e:
+        run_command(["firewall-cmd", "--add-service=tor", "--permanent"])
+        run_command(["firewall-cmd", "--reload"])
+    except Exception as e:
         print(f"Ошибка при установке Tor: {e}")
         sys.exit(1)
 
 def start_tor_service():
     print("Запускаю сервис Tor...")
-    try:
-        subprocess.run(["systemctl", "enable", "--now", "tor"], check=True)
-        print("Сервис Tor запущен.")
-    except subprocess.CalledProcessError as e:
-        print(f"Ошибка при запуске сервиса Tor: {e}")
-        sys.exit(1)
+    run_command(["systemctl", "enable", "--now", "tor"])
+    print("Сервис Tor запущен.")
 
 def check_tor_status():
     try:
