7d2d4300438fe63557089b75d094518c067de63b
Edd <33794654+giteed@users.noreply.github.com>
2025-01-13 17:42:32 +0300

    Update cognitive_interface_agent.py

--- agents/cognitive_interface_agent.py
+++ agents/cognitive_interface_agent.py
@@ -2,7 +2,7 @@
 # LLMCAN/agents/cognitive_interface_agent.py
 # ==================================================
 # Когнитивный интерфейсный агент для проекта LLMCAN
-# Версия: 2.4
+# Версия: 2.5
 # ==================================================
 
 import sys
@@ -16,6 +16,7 @@ sys.path.insert(0, str(project_root))
 
 from settings import BASE_DIR, LLM_API_URL
 from cognitive_interface_agent_functions import *
+from agents.install_tor import restart_tor_and_check_ddgr
 
 # Обновленный класс Colors
 class Colors:
