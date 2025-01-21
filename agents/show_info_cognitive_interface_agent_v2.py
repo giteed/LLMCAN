834a66fa94ba4f81adb4ec020a6cc493556276d5
Edd <33794654+giteed@users.noreply.github.com>
2025-01-21 21:53:07 +0300

    Update show_info_cognitive_interface_agent_v2.py

--- agents/show_info_cognitive_interface_agent_v2.py
+++ agents/show_info_cognitive_interface_agent_v2.py
@@ -14,8 +14,8 @@ from settings import (
     LLM_API_HEALTH,   # для check_llm_api_status
     LLM_API_TAGS,     # для get_ollama_models
     LLM_API_GENERATE  # для test_ollama_query
-    LLM_API_URL       # LLM_API_URL
-)
+    LLM_API_URL       # LLM_API_URL)
+
 
 def get_ip_address():
     """Получает локальный IP-адрес."""
