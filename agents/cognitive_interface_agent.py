d392f5f0b25522b4b5ee9b8be32b3460b71e1e0b
Edd <33794654+giteed@users.noreply.github.com>
2025-01-22 00:08:51 +0300

    Update cognitive_interface_agent.py

--- agents/cognitive_interface_agent.py
+++ agents/cognitive_interface_agent.py
@@ -14,7 +14,7 @@ import subprocess
 project_root = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(project_root))
 
-from settings import BASE_DIR, LLM_API_URL
+from settings import BASE_DIR, LLM_API_GENERATE
 from cognitive_interface_agent_functions import *
 from agents.install_tor import restart_tor_and_check_ddgr
 
