92b34c36c108176c4b0c58c6f7294ab1af42096f
Edd <33794654+giteed@users.noreply.github.com>
2025-01-21 23:28:56 +0300

    Update show_info_cognitive_interface_agent_v2.py

--- agents/show_info_cognitive_interface_agent_v2.py
+++ agents/show_info_cognitive_interface_agent_v2.py
@@ -11,7 +11,6 @@ from pathlib import Path
 from colors import Colors
 import requests
 import socket
-#from settings import LLM_API_URL
 
 # LLM_API_HEALTH для check_llm_api_status.
 # LLM_API_TAGS, # для get_ollama_models.
